798c5a9c9f758e839582ac3535d0af641f909ff8
vivekhebbar <vhebbar13@gmail.com>
2016-11-02 02:13:31 -0700

    starting to add choice js frontend stuff, tedious conversion from old repo bc not well documented

--- templates/chatbot/populate.js
+++ templates/chatbot/populate.js
@@ -1,27 +1,46 @@
+var id = 'forPopulate';
+
 function initial() {
-	var id = 'forPopulate';
-	var nT = '<div class="form-group"><input type="text" class="form-control" id="name" placeholder = "Hi! I\'m Ludo. Enter your name!" autocomplete="off" onkeydown="if (event.keyCode == 13) { takeName(); return false; }"><h2 id="namesubmit" onclick="takeName()">Submit</h2></div>';
+	var nT = '<div class="form-group"><input type="text" class="form-control" id="name" placeholder = "Hi! I\'m Ludo. Enter your name!" autocomplete="off" autofocus="autofocus" onkeydown="if (event.keyCode == 13) { takeName(); return false; }"><h2 id="pressenter" onclick="takeName()">Enter</h2></div>';
 	changeById(id, nT);
 }
 
 function takeName() {
 	var nameInput = document.getElementById('name');
 	var userName = nameInput.value;
-	var id = 'forPopulate';
 	changeById(id, "");
 	start(userName);
 }
 
 function start(name) {
-	var id = 'forPopulate';
 	var str1 = '<h2>Well hey there,  ' + name + '! Nice to meet you.</h2>';
+	changeById(id, str1);
 	var str2 = '<h2>My name\'s Ludo, and I\'m here for you if you\'re bored and want something to do...</h2></br>';
-	var strbtn = '<h2 id="im-game" onclick=next()>' + '<i class="fa fa-arrow-circle-right"></i>&nbsp;&nbsp;&nbsp;&nbsp;' + "I'M GAME" +'&nbsp;&nbsp;&nbsp;&nbsp;</h2>';
-	//changeById(id, str1);
-	//changeById(id, str1 + str2);
-	changeById(id, str1 + str2 + strbtn);
+	var strbtn = '<h2 id="im-game" onclick=next(name)>' + '<i class="fa fa-arrow-circle-right"></i>&nbsp;&nbsp;&nbsp;&nbsp;' + "I'M GAME" +'&nbsp;&nbsp;&nbsp;&nbsp;</h2>';
+	changeById(id, str2 + strbtn);
 }
 
-function next() {
-	alert("Hey");
+function next(name) {
+	choices();
+	
+}
+
+
+function choices() {
+	choiceList = bogusChoices();
+	labels = choiceList[0]
+	probs = choiceList[1]
+	alert(labels);
+}
+
+function bogusChoices() {
+	arr1 = ['Steel', 'Coal', 'Mining', 'Education', 'Medical', 'Law']
+	arr2 = [1 , 2, 3, 1, 2, 4]
+	var sum2 = arr2.reduce(function(a,b) { return a+ b;}, 0);
+	for(var i=0; i<arr2.length; i++) {
+    	arr2[i] /= sum2;
+	}
+	return [arr1, arr2];
+
+
 }
--- templates/chatbot/transitions.js
+++ templates/chatbot/transitions.js
@@ -1,9 +1,9 @@
 function changeById(id, newText) {
 	var c = document.getElementById(id);
-	c.style.transition = "all 1s";
-	c.style.WebKitTransition = "all 1s";
-	c.style.opacity = 0;
-	c.innerHTML = newText;
-	setTimeout(function(){c.style.opacity = 1;}, 1000);
-	c.innerHTML = newText;
+	c.style.transition = "all 0.5s";
+	c.style.WebKitTransition = "all 0.5s";
+	setTimeout(function(){c.style.opacity = 0;}, 500);
+	setTimeout(function(){c.style.opacity = 1;}, 999);
+	setTimeout(function(){c.innerHTML = newText;}, 1000);
+
 }
