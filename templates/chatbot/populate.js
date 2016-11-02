f108319b7fa9ef49b87000a69ee4831857ca0d53
vivekhebbar <vhebbar13@gmail.com>
2016-11-02 01:41:00 -0700

    more transition effects on js side

--- templates/chatbot/populate.js
+++ templates/chatbot/populate.js
@@ -1,6 +1,6 @@
-function init() {
+function initial() {
 	var id = 'forPopulate';
-	var nT = '<div class="form-group"><input type="text" class="form-control" id="name" placeholder = "Hi! I\'m Ludo. Enter your name!" autocomplete="off"><h2 id="namesubmit" onclick="takeName()">Submit</h2></div>';
+	var nT = '<div class="form-group"><input type="text" class="form-control" id="name" placeholder = "Hi! I\'m Ludo. Enter your name!" autocomplete="off" onkeydown="if (event.keyCode == 13) { takeName(); return false; }"><h2 id="namesubmit" onclick="takeName()">Submit</h2></div>';
 	changeById(id, nT);
 }
 
@@ -14,6 +14,14 @@ function takeName() {
 
 function start(name) {
 	var id = 'forPopulate';
-	var strname = "hello " + name;
-	changeById(id, "<h2>" + strname + "</h2>");
+	var str1 = '<h2>Well hey there,  ' + name + '! Nice to meet you.</h2>';
+	var str2 = '<h2>My name\'s Ludo, and I\'m here for you if you\'re bored and want something to do...</h2></br>';
+	var strbtn = '<h2 id="im-game" onclick=next()>' + '<i class="fa fa-arrow-circle-right"></i>&nbsp;&nbsp;&nbsp;&nbsp;' + "I'M GAME" +'&nbsp;&nbsp;&nbsp;&nbsp;</h2>';
+	//changeById(id, str1);
+	//changeById(id, str1 + str2);
+	changeById(id, str1 + str2 + strbtn);
+}
+
+function next() {
+	alert("Hey");
 }
--- templates/chatbot/transitions.js
+++ templates/chatbot/transitions.js
@@ -1,25 +1,9 @@
-function hasClass (e, classname) {
-    return e.className.match(new RegExp('(\\s|^)' + classname + '(\\s|$)'));
-}
-// Add a class
-function addClass (e, classname) {
-    if (!hasClass(e, classname)) { e.className += " " + classname };
-}
-// Remove a class
-function removeClass (e, classname) {
-    if (hasClass(e, classname)) {
-        var reg = new RegExp('(\\s|^)' + classname + '(\\s|$)');
-        e.className = e.className.replace(reg,' ');
-    }       
-}
-
 function changeById(id, newText) {
 	var c = document.getElementById(id);
-	// Transitions only happen when a property changes
-	addClass(c, "invisible");
-	// After 1.5s, change innerHTML and fade-in
-	setTimeout(function () {
-	    c.innerHTML = newText;
-	    removeClass(c, "invisible");
-	}, 1500);
+	c.style.transition = "all 1s";
+	c.style.WebKitTransition = "all 1s";
+	c.style.opacity = 0;
+	c.innerHTML = newText;
+	setTimeout(function(){c.style.opacity = 1;}, 1000);
+	c.innerHTML = newText;
 }
