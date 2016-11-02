c1cddcb4bb104ccd8f3eabfed8115f3516683290
vivekhebbar <vhebbar13@gmail.com>
2016-11-02 02:50:30 -0700

    finally all transitions are proper

--- templates/chatbot/populate.js
+++ templates/chatbot/populate.js
@@ -6,12 +6,12 @@ window.onload = function() {initial();};
 function initial() {
 	var str1 = '<a href="index.html">WELCOME TO LUDO</a>'
 	var nexttime = changeById(idTitle, str1, 0);
-	var nT = '<h2>Ludo is a chatbot that will recommend you activities for you to enjoy!</h2></br><h2 onclick="showInput()">Click here to begin&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-arrow-circle-down"></i></h2>';
-	changeById(idPopulate, nT, nexttime + 2000);
+	var nT = '<h2>LuDO is a chatbot that will recommend you activities for you to enjoy!</h2></br><h2 onclick="showInput()">Click here to begin&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-arrow-circle-down"></i></h2>';
+	changeById(idPopulate, nT, nexttime + 1500);
 }
 
 function showInput(){
-	var nT = '<div class="form-group"><input type="text" class="form-control" id="name" placeholder = "Hi! I\'m Ludo. Enter your name!" autocomplete="off" autofocus="autofocus" onkeydown="if (event.keyCode == 13) { takeName(); return false; }"><h2 id="pressenter" onclick="takeName()">Enter</h2></div>';
+	var nT = '<div class="form-group"><input type="text" class="form-control" id="name" placeholder = "Hi! Give me your name/nickname" autocomplete="off" autofocus="autofocus" onkeydown="if (event.keyCode == 13) { takeName(); return false; }"><h2 id="pressenter" onclick="takeName()">ENTER</h2></div>';
 	changeById(idPopulate, nT, 0);
 }
 
@@ -25,7 +25,7 @@ function takeName() {
 function start(name) {
 	var str1 = '<h2>Well hey there,  ' + name + '! Nice to meet you.</h2>';
 	var nexttime = changeById(idPopulate, str1, 0);
-	var str2 = '<h2>My name\'s Ludo, and I\'m here for you if you\'re bored and want something to do...</h2></br>';
+	var str2 = '<h2>My name\'s LuDO, and I\'m here for you if you\'re bored and want something to do...</h2></br>';
 	var strbtn = '<h2 id="im-game" onclick=next(name)>' + '<i class="fa fa-arrow-circle-right"></i>&nbsp;&nbsp;&nbsp;&nbsp;' + "I'M GAME" +'&nbsp;&nbsp;&nbsp;&nbsp;</h2>';
 	changeById(idPopulate, str2 + strbtn, nexttime + 2000);
 }
