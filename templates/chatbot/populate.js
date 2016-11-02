c6b5c34acc5e98622ec0d10354e1f82cae6ffeee
vivekhebbar <vhebbar13@gmail.com>
2016-11-02 00:12:14 -0700

    slowly adding fnality

--- templates/chatbot/populate.js
+++ templates/chatbot/populate.js
@@ -1,7 +1,7 @@
 function init() {
 	var forPop = document.getElementById('forPopulate');
 	forPop.innerHTML = "";
-	forPop.innerHTML = '<div class="form-group"><input type="text" class="form-control" id="name" placeholder = "Hi! I\'m Ludo. Enter your name!"><id="namesubmit" class="h2" onclick="takeName()">Submit</button></div>';
+	forPop.innerHTML = '<div class="form-group"><input type="text" class="form-control" id="name" placeholder = "Hi! I\'m Ludo. Enter your name!"><h2 id="namesubmit" onclick="takeName()">Submit</h2></div>';
 }
 
 function takeName() {
@@ -9,4 +9,13 @@ function takeName() {
 	var userName = nameInput.value;
 	var forPop = document.getElementById('forPopulate');
 	forPop.innerHTML = "";
+	forPop.innerHTML = ""
+	start(userName);
+}
+
+function start(name) {
+	var forPop = document.getElementById('forPopulate');
+	forPop.innerHTML = "<h2>hello</h2>"
+	// var strHTML = "<div class="container"><h2>A</h2></div>"
+	// forPop.innerHTML = strHTML;
 }
