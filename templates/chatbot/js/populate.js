cbf17a3082d632d8ba4b08a5549b334ea659531f
vivekhebbar <vhebbar13@gmail.com>
2016-11-02 13:09:22 -0700

    whoops

--- templates/chatbot/js/populate.js
+++ templates/chatbot/js/populate.js
@@ -6,61 +6,85 @@ var user_dict = {};
 window.onload = function() {initial();};
 
 function initial() {
+	//display website welcome sign
 	var str1 = '<a href="index.html">WELCOME TO LuDO</a>'
-	var nexttime = changeById(idTitle, str1, 0);
+	var nexttime = changeOpacityById(idTitle, str1, 0);
+	//display subtitle
 	var nT = '<h2>LuDO is a chatbot that will recommend you activities for you to enjoy!</h2></br><h2 onclick="showInput()">Click here to begin&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-arrow-circle-down"></i></h2>';
-	changeById(idPopulate, nT, nexttime + 2000);
+	changeOpacityById(idPopulate, nT, nexttime + 1000);
 }
 
 function showInput(){
-	var nT = '<div class="form-group"><input type="text" class="form-control" id="name" placeholder = "Hi! What\'s your name?" autocomplete="off" autofocus="autofocus" onkeydown="if (event.keyCode == 13) { takeName(); return false; }"><h2 id="pressenter" onclick="takeName()">ENTER</h2></div>';
-	changeById(idPopulate, nT, 0);
+	//Called from initial. display input bar for user name entry
+	var nT = '<div class="form-group"><input autofocus type="text" class="form-control" id="name" placeholder = "Hi! What\'s your name?" autocomplete="off" onkeydown="if (event.keyCode == 13) { takeName(); return false; }"><h2 id="pressenter" onclick="takeName()">ENTER</h2></div>';
+	changeOpacityById(idPopulate, nT, 0);
 }
 
 function takeName() {
+	//Called from name entry. get name username from id name; put it in user_dict
 	var nameInput = document.getElementById('name');
 	var userName = nameInput.value;
 	user_dict.name = userName;
-	changeById(idPopulate, "", 0);
-	start(userName);
+	changeOpacityById(idPopulate, "", 0);
+	start();
 }
 
-function start(name) {
-	var str1 = '<h2>Well hey there,  ' + name + '! Nice to meet you.</h2>';
-	var nexttime = changeById(idPopulate, str1, 0);
+function start() {
+	//Called after username determined.start conversation with user_dict.name
+	var str1 = '<h2>Well hey there,  ' + user_dict.name + '! Nice to meet you.</h2>';
+	var nexttime = changeOpacityById(idPopulate, str1, 0);
 	var str2 = '<h2>My name\'s LuDO, and I\'m here for you if you\'re bored and want something to do...</h2></br>';
-	var strbtn = '<h2 id="im-game" onclick=askInitialQueries(name)>' + '<i class="fa fa-arrow-circle-right"></i>&nbsp;&nbsp;&nbsp;&nbsp;' + "I'M GAME" +'&nbsp;&nbsp;&nbsp;&nbsp;</h2>';
-	changeById(idPopulate, str2 + strbtn, nexttime + 2000);
+	var strbtn = '<h2 id="im-game" onclick=askInitialLocationQuery()>' + '<i class="fa fa-arrow-circle-right"></i>&nbsp;&nbsp;&nbsp;&nbsp;' + "I'M GAME" +'&nbsp;&nbsp;&nbsp;&nbsp;</h2>';
+	changeOpacityById(idPopulate, str2 + strbtn, nexttime + 1000);
 }
 
-function askInitialQueries(name) {
-	//determine user's desired activity location.
-	var userQuery = '<h2>Ok, ' + user_dict.name + ', I am going to need to ask a couple questions. </h2>';
-	userQuery += '<h2>What location do you want to explore for activities?</h2></br>'
-	var nT = '<div class="form-group"><input type="text" class="form-control" autofocus="autofocus" id="location" placeholder = "Enter Location and Press Enter" autocomplete="off" onkeydown="if (event.keyCode == 13) { takeLocation(); return false; }"><h2 id="pressenter" onclick="takeLocation()">ENTER</h2></div>';
-	changeById(idPopulate, userQuery + nT, 0);
+function askInitialLocationQuery() {
+	//Called on I'm Game click.User wants to play. determine user's desired 
+	//activity location.
+	var userQuery = '<h2>Ok, ' + user_dict.name + ', what location do you';
+	userQuery += ' want to explore for activities?</h2></br>';
+	var nT = '<div class="form-group"><input autofocus="autofocus" type="text"';
+	nT += ' class="form-control" id="location" placeholder = "Enter Location"';
+	nT += ' autocomplete="off" onkeydown="if (event.keyCode == 13) { takeLocation();';
+	nT += ' return false; }"><h2 id="locationenter" onclick="takeLocation()">ENTER</h2></div>';
+	var nexttime = changeOpacityById(idPopulate, userQuery + nT, 0);
+
 }
 
 function takeLocation() {
+	//Called on location enter. collect input from location text input and 
+	//use Google Maps API to reverse geocode the input. This gives us a 
+	//latitude and longitude if valid.
 	var locationInput = document.getElementById('location');
 	var location = locationInput.value;
 	var location_str = location.split(" ").join("+")
 	var url = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 	url += location_str + '&key=' + api_token;
-	console.log(url);
 	var xmlHttp = new XMLHttpRequest();
 	xmlHttp.onreadystatechange = function() {
 		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
 			var json_dct = eval("(" + xmlHttp.responseText + ")");
 			var results = json_dct.results[0];
+			user_dict.address = results.formatted_address;
 			user_dict.lat = results.geometry.location.lat;
 			user_dict.lng = results.geometry.location.lng;
-			loc_str = "Latitude: " + user_dict.lat + " Longitude: " + user_dict.lng;
-			changeById(idPopulate, loc_str, 0);
+			//changeOpacityById(idPopulate, user_dict.lng, 0);
+			askInitialTimeQuery();
 		}
 	}
 	xmlHttp.open('GET', url, true);
 	xmlHttp.send(null);
+}
+
+function askInitialTimeQuery() {
+	//called from AskInitialLocationQuery() as it is the next round of initial
+	//questioning. Now the user is asked what time frame he is looking for.
+	var answer1 = '<h2>' + user_dict.address + ' sounds great!</h2></br>'
+	answer1 += ' What time frame were you thinking?';
+	answer1 += ' Pick one of the following options:';
+	var svgHtml = '<svg width="500" height="600"></svg>';
+	document.getElementById(idPopulate).innerHTML = answer1 + svgHtml;
+	bubbleCSV('csv/init1.csv');
 }
 
 function queryDecisionTree(){
